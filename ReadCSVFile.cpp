cd3cceb7f60d43e57815e7c4e6f66cdb4d63feb5
jtsdev2023 <jtsdev2023@gmail.com>
2024-08-11 19:17:03 +0000

    added csv reader to read smart device info from file. print csv to std::cout

--- ReadCSVFile.cpp
+++ ReadCSVFile.cpp
@@ -3,17 +3,25 @@
 
 
 #include "csv.h"
+#include <iomanip>
 #include <iostream>
 
 void readCSVFile() {
-    io::CSVReader<3> in("csv01.csv");
-    in.read_header(io::ignore_extra_column, "header_1", "header_2", "header_3");
-    std::string header_1; std::string header_2; std::string header_3;
+    std::string header_1 = "Device Name";
+    std::string header_2 = "Serial Number";
+    std::string header_3 = "License Number";
 
-    std::cout << "header_1     " << "header_2     " << "header_3     " << std::endl;
-    while(in.read_row(header_1, header_2, header_3))
+    io::CSVReader<3> in("csv01.csv");
+    in.read_header(io::ignore_extra_column, header_1, header_2, header_3);
+    std::string h_1; std::string h_2; std::string h_3;
+
+    // std::cout << header_1 << "  " << header_2 << "  " << "  " << header_3 <<  std::endl;
+    while(in.read_row(h_1, h_2, h_3))
     {
         // write output
-        std::cout << header_1 << "     " << header_2 << "     " << "     " << header_3 << "     " <<  std::endl;
+        std::cout << header_1 << ":      " << h_1 << std::endl;
+        std::cout << header_2 << ":    " << h_2 << std::endl;
+        std::cout << header_3 << ":   " << h_3 << std::endl;
+        std::cout << std::endl;
     }
 }
